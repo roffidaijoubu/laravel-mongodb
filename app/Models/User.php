3d7115131f783e0bf136dd002c4322d9b5994c91
Roffi Daijoubu <roffidaijoubu@gmail.com>
2023-12-09 17:29:02 +0700

    spatie-permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use MongoDB\Laravel\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
